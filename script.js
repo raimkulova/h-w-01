c06107d1102aa41f0718d4e1e2e3c9f5c2a118e3
raimkulova <105550553+raimkulova@users.noreply.github.com>
2022-06-03 20:11:39 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let a = 10;
+
+let b = 3;
+
+let c = a % b;
+console.log(c);
+
+let st = 2 ** 10;
+console.log(st);
+
+let d = '1';
+
+let e = 2;
+
+let x = 'белых медведей';
+
+console.log(d + e ,x );
+
+let y = 5; 
+
+let p = 10;
+
+let o ;
+
+ o = ++y + --p; 
+
+ console.log(o);
+
+ const s = 15 * (4 + 25 - 55);
+
+ console.log(s);
+
